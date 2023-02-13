abb513d6e7f4b5013536df907af1de04942a80f4
Charlese <charles.easton123@gmail.com>
2023-02-13 00:12:03 -0500

    add more semantic hmtl to summary

--- frontend/src/components/paymentContainer.jsx
+++ frontend/src/components/paymentContainer.jsx
@@ -1,59 +1,54 @@
-import React from 'react'
+import React from "react";
 
 function paymentContainer() {
+
+  const calculate = () => {
+    console.log("hello");
+  }
+
   return (
     <div>
-        <h1> Payment Plan</h1>
-        <div className='rowGroup'>
-            <label className = 'MortgageAmount'>
-            Mortgage Amount
-            </label>
-            <span className='icon'>$</span>
-            <input type="text"></input>
-        </div>
-        <div className='rowGroup'>
-            <label className = 'InterestRate'>
-            Interest Rate
-            </label>
-            <span className='icon'>%</span>
-            <input type="text"></input>
-        </div>
-        <div className='rowGroup'>
-            <label className = 'AmortizationPeriod'>
-            Amortization Period
-            </label>
-            <input type="text"></input>
-        </div>
-        <div className='rowGroup'>
-            <label className = 'Payment Frequency'>
-            Payment Frequency
-            </label>
-            <select name="frequency">
-    <option value="weekly">Weekly</option>
-    <option value="bi-weekly">Bi-weekly</option>
-    <option value="monthly">Monthly</option>
-
-  </select>
-        </div>
-        <div className='rowGroup'>
-            <label className = 'Term'>
-            Term
-            </label>
-            <select name="Terms">
-    <option value="1 Year">1 Year</option>
-    <option value="2 Year">2 Year</option>
-    <option value="3 Year">3 Year</option>
-    <option value="4 Year">4 Year</option>
-    <option value="5 Year">5 Year</option>
-    <option value="6 Year">6 Year</option>
-    <option value="7 Year">7 Year</option>
-    <option value="8 Year">8 Year</option>
-    <option value="9 Year">9 Year</option>
-    <option value="10 Year">10 Year</option>
-
-  </select>        </div>
+      <h1> Payment Plan</h1>
+      <div className="rowGroup">
+        <label className="MortgageAmount">Mortgage Amount</label>
+        <span className="icon">$</span>
+        <input type="text"></input>
+      </div>
+      <div className="rowGroup">
+        <label className="InterestRate">Interest Rate</label>
+        <span className="icon">%</span>
+        <input type="text"></input>
+      </div>
+      <div className="rowGroup">
+        <label className="AmortizationPeriod">Amortization Period</label>
+        <input type="text"></input>
+      </div>
+      <div className="rowGroup">
+        <label className="Payment Frequency">Payment Frequency</label>
+        <select name="frequency">
+          <option value="weekly">Weekly</option>
+          <option value="bi-weekly">Bi-weekly</option>
+          <option value="monthly">Monthly</option>
+        </select>
+      </div>
+      <div className="rowGroup">
+        <label className="Term">Term</label>
+        <select name="Terms">
+          <option value="1 Year">1 Year</option>
+          <option value="2 Year">2 Year</option>
+          <option value="3 Year">3 Year</option>
+          <option value="4 Year">4 Year</option>
+          <option value="5 Year">5 Year</option>
+          <option value="6 Year">6 Year</option>
+          <option value="7 Year">7 Year</option>
+          <option value="8 Year">8 Year</option>
+          <option value="9 Year">9 Year</option>
+          <option value="10 Year">10 Year</option>
+        </select>{" "}
+      </div>
+      <button onClick={calculate}>Calculate</button>
     </div>
-  )
+  );
 }
 
-export default paymentContainer
+export default paymentContainer;
--- frontend/src/components/summary.jsx
+++ frontend/src/components/summary.jsx
@@ -1,88 +1,51 @@
-import React from 'react'
+import React from "react";
 
 function summary() {
   return (
     <div>
+      <table className='summaryTable'>
+        <caption>Calculation Summary</caption>
+        <thread>
+          <tr>
+            <th >Category</th>
+            <th>Term</th>
+            <th>Amortization Period</th>
+          </tr>
+        </thread>
+        <tbody>
+          <tr>
+            <td>Number of Payments</td>
+            <td>5</td>
+            <td>200</td>
+          </tr>
 
-<table>
-<caption>Calculation Summary</caption>
+          <tr>
+            <td>Mortgage Payments</td>
+            <td>5</td>
+            <td>200</td>
+          </tr>
 
-                <tr>
-        <th>
-        Category
-        </th>
-        <th>
-        Term
-        </th>
-        <th>
-        Amortization Period
-        </th>
-        </tr>
+          <tr>
+            <td>Prepayment</td>
+            <td>5</td>
+            <td>200</td>
+          </tr>
 
-    <tr>
-    <td>
-    Number of Payments
-    </td>
-    <td>
-    5
-    </td>
-    <td>
-    200
-    </td>
-    </tr>
+          <tr>
+            <td>Interest Payments</td>
+            <td>5</td>
+            <td>200</td>
+          </tr>
 
-    <tr>
-    <td>
-    Mortgage Payments
-    </td>
-     <td>
-    5
-    </td>
-    <td>
-    200
-    </td>
-    </tr>
-
-    <tr>
-    <td>
-    Prepayment
-    </td>
-    <td>
-    5
-    </td>
-    <td>
-    200
-    </td>
-    </tr>
-
-    <tr>
-    <td>
-    Interest Payments
-    </td>
-    <td>
-    5
-    </td>
-    <td>
-    200
-    </td>
-    </tr>
-
-    <tr>
-<td>
-    Total Costs
-    </td>
-    <td>
-    5
-    </td>
-    <td>
-    200
-    </td>
-    </tr>
-    
-    </table>
+          <tr>
+            <td>Total Costs</td>
+            <td>5</td>
+            <td>200</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-
-  )
+  );
 }
 
-export default summary
+export default summary;
